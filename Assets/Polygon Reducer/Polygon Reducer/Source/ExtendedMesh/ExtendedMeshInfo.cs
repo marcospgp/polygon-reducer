2024bc00ca3a8f5a9fa514792d6a78ea426b246f
Marcos Pereira <marcospereiradev@gmail.com>
2022-02-03 16:29:29 +0000

    asdf

--- Assets/Polygon Reducer/Polygon Reducer/Source/ExtendedMesh/ExtendedMeshInfo.cs
+++ Assets/Polygon Reducer/Polygon Reducer/Source/ExtendedMesh/ExtendedMeshInfo.cs
@@ -1,14 +1,22 @@
+using UnityEngine;
+
 namespace MarcosPereira.MeshManipulation {
     // This class is used to display ExtendedMesh information in the inspector.
     // It is easier than serializing the ExtendedMesh class itself.
     [System.Serializable]
     public class ExtendedMeshInfo {
-        public string originalMeshName;
-        public int originalVertexCount;
-        public int reducedVertexCount;
-        public int originalTriangleCount;
-        public int reducedTriangleCount;
-        public int seamVertexCount;
+        [SerializeField]
+        private string originalMeshName;
+        [SerializeField]
+        private int originalVertexCount;
+        [SerializeField]
+        private int reducedVertexCount;
+        [SerializeField]
+        private int originalTriangleCount;
+        [SerializeField]
+        private int reducedTriangleCount;
+        [SerializeField]
+        private int seamVertexCount;
 
         public ExtendedMeshInfo(ExtendedMesh extendedMesh) {
             this.originalMeshName = extendedMesh.originalMesh.name;
